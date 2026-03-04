f8300138e37a47e0cbc2b8a8f5cb3446799c3412
Rafly13-cmd <muhammadraflymukti13@gmail.com>
2026-03-04 10:16:56 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_4P5gD77uHd9M5xghHLM4Twii7tjDTCQ",
+    authDomain: "web-fti-db708.firebaseapp.com",
+    projectId: "web-fti-db708",
+    storageBucket: "web-fti-db708.appspot.com",
+    messagingSenderId: "225122563478",
+    appId: "1:225122563478:web:18f877ac98be41fa7659b7",
+    measurementId: "G-HX7PXKRLEL"
+  };
+
+  const app = firebase.initializeApp(firebaseconfig)
