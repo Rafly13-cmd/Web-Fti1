11a3da7b9917f0b08cf78aceb368ceb7d82bb708
Rafly13-cmd <muhammadraflymukti13@gmail.com>
2026-03-06 09:39:27 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB_4P5gD77uHd9M5xghHLM4Twii7tjDTCQ",
+    apiKey: 
     authDomain: "web-fti-db708.firebaseapp.com",
     projectId: "web-fti-db708",
     storageBucket: "web-fti-db708.appspot.com",
@@ -8,5 +8,6 @@ const firebaseConfig = {
     appId: "1:225122563478:web:18f877ac98be41fa7659b7",
     measurementId: "G-HX7PXKRLEL"
   };
+
 
   const app = firebase.initializeApp(firebaseconfig)
